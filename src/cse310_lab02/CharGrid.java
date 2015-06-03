55894cdb73b6dbe0b74aabdaa44150edb72c9ac5
rayedbar <mohammedrayed@gmail.com>
2015-06-03 12:41:32 +0600

    first commit

---
+++ src/cse310_lab02/CharGrid.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cse310_lab02;
+
+/**
+ *
+ * @author Rayed
+ */
+public class CharGrid {
+    
+    private char [][] grid = new char [][] {
+        {'a', 'b', 'c', 'd'},
+        {'a', ' ', 'c', 'b'},
+        {'x', 'b', 'c', 'a'},
+        {'x', 'b', ' ', 'x'}
+    };
+    
+    public int charArea (char ch){
+        int area = 0; 
+        
+        
+        
+        return area;
+    }
+    
+}
---
+++ src/cse310_lab02/Main.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cse310_lab02;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Stack;
+import java.util.Scanner;
+/**
+ *
+ * @author Rayed
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String to Blow Up:" );
+        blowUp(sc.nextLine());
+        System.out.println("Enter String to Find Run:" );
+        maxRun(sc.nextLine());
+    }
+
+    private static void blowUp (String s){
+        String s1 = ""; 
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); ++i){
+            char c = s.charAt(i);
+            if (Character.isDigit(c)){
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }
+                stack.push(Character.getNumericValue(c));
+            } else {
+                if (!stack.isEmpty()){
+                    int j = stack.pop();
+                    for (int k = 0; k < j; k++){
+                        s1 += c;
+                    }
+                } else {
+                    s1 += c;
+                }
+            }
+        }
+        
+        System.out.println(s1);
+    }
+
+    private static void maxRun (String s) {
+        int run;
+        char [] array = s.toCharArray();
+        Map<Character, Integer> map = new HashMap<>();
+        
+        for (Character c : array){
+            if (map.containsKey(c)){
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        
+        run = Collections.max(map.values());
+        
+        for (Entry<Character, Integer> entry : map.entrySet()){
+            if (entry.getValue() == run){
+                System.out.println(entry.getKey());
+            }
+        }
+    }
+}
